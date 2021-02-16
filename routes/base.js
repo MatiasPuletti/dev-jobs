738c84e9c0a7a0ef6f5b148f3bfab6a8a0fa7f64
eguiadaniel <eguiadaniel@users.noreply.github.com>
2021-02-16 20:58:10 +0100

    sacrh bar for title and multiple words

--- routes/base.js
+++ routes/base.js
@@ -19,19 +19,30 @@ router.get('/', (req, res, next) => {
 
 // Search jobs
 
+//{ $regex: /search/i }
+
 router.get('/search', (req, res, next) => {
   const search = req.query.search;
+  const terms = search.split(' ');
+  Job.find({
+    // title: new RegExp('\\b' + search + '\\b', 'i')
+    $and: terms.map((term) => ({
+      title: new RegExp('\\b' + term + '\\b', 'i')
+    }))
+    // title: new RegExp('\\b' + search + '\\b', 'i')
+  }).then((jobs) => {
+    res.render('home', { job: jobs });
+  });
+  // Job.find({ title: search })
+  //   .then((job) => {
+  //     console.log(search);
 
-  Job.find({ skill: search })
-    .then((job) => {
-      console.log(search);
-
-      console.log(job);
-      res.render('home', { job });
-    })
-    .catch((error) => {
-      next(error);
-    });
+  //     console.log(job);
+  //     res.render('home', { job });
+  //   })
+  //   .catch((error) => {
+  //     next(error);
+  //   });
 });
 
 router.get('/private', (req, res, next) => {
