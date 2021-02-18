12fba41658dc6b92cfbcc4e3d9f83a6caf8c1dae
eguiadaniel <eguiadaniel@users.noreply.github.com>
2021-02-18 21:55:04 +0100

    mixing search bar and filters on home

--- routes/base.js
+++ routes/base.js
@@ -20,10 +20,64 @@ router.get('/', (req, res, next) => {
 // Search jobs multiple words
 
 router.get('/search', (req, res, next) => {
-  const search = req.query.search;
-  // const skillQuery = req.query.skill || [];
+  const search = req.query.q;
+  const skillQuery = req.query.skill || [
+    'javascript',
+    'css',
+    'html',
+    'mongodb',
+    'canvas',
+    'react',
+    'vue'
+  ];
   // console.log(skillQuery);
   const terms = search.split(' ');
+  Job.find({
+    $or: [
+      {
+        $and: [
+          { title: search }
+          //{ title: 'test' },
+        ]
+      },
+      {
+        $or: [{ skill: skillQuery }]
+      }
+    ]
+  })
+
+    /*$and: terms.map((term) => ({
+      title: new RegExp('\\b' + term + '\\b', 'i')
+    }))
+    ,skill: { $in: skillQuery }
+    */
+    .then((jobs) => {
+      res.render('home', { job: jobs, search });
+    });
+});
+
+//search jobs filter checkboxes
+/*
+router.get('/filter', (req, res, next) => {
+  console.log(searchQuery);
+  console.log(skillQuery);
+  console.log(searchResults);
+  const search = req.body.q;
+  console.log("HERE",search)
+  const skills = ['css', 'html', 'mongodb'];
+  const terms = search.split(' ');
+  // const skillQuery = req.query.skill || [];
+  // console.log(skillQuery);
+  let ze = terms.map((term) => ({
+    title: new RegExp('\\b' + term + '\\b', 'i')
+  }));
+  //let dan = `, skill: [${skills}]`;
+  let dan = skills;
+  let full = ze + dan;
+  console.log(ze);
+  console.log(dan);
+  console.log(full);
+
   Job.find({
     $and: terms.map((term) => ({
       title: new RegExp('\\b' + term + '\\b', 'i')
@@ -32,26 +86,10 @@ router.get('/search', (req, res, next) => {
     //skill: { $in: skillQuery }
   }).then((jobs) => {
     res.render('home', { job: jobs });
-    }) 
+    searchResults = jobs;
+  });
 });
-
-//search jobs filter checkboxes
-
-router.get('/filter', (req, res, next) => {
-  const query = req.query.q;
-  const skillQuery = req.query.skill;
-
-  Job.find({ skill: skillQuery })
-    .then((job) => {
-      //console.log(search);
-      //console.log(job);
-      res.render('home', { job });
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
-
+*/
 router.get('/private', (req, res, next) => {
   if (req.user) {
     res.render('private');
